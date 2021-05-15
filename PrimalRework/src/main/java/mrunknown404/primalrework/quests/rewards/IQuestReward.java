24f22de8b4391a394396f6f341f1d9e453aa0956
-Unknown- <mrunknownyt@gmail.com>
2021-05-15 16:40:35 -0600

    Fresh start

--- PrimalRework/src/main/java/mrunknown404/primalrework/quests/rewards/IQuestReward.java
+++
@@ -1,7 +0,0 @@
-package mrunknown404.primalrework.quests.rewards;
-
-import net.minecraft.entity.player.EntityPlayer;
-
-public interface IQuestReward {
-	public void giveRewardsToPlayer(EntityPlayer player);
-}
