83d4739da1dcca5b41f50e1258865192a32fdfe4
-Unknown- <mrunknownyt@gmail.com>
2022-07-04 09:52:55 -0600

    LeafEvent rework

--- PrimalRework/src/main/java/mrunknown404/primalrework/events/LeafEvents.java
+++ PrimalRework/src/main/java/mrunknown404/primalrework/events/LeafEvents.java
@@ -1,5 +1,8 @@
 package mrunknown404.primalrework.events;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -7,8 +10,6 @@ import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 
 import mrunknown404.primalrework.blocks.SBLeaves;
-import mrunknown404.primalrework.utils.LeafTickScheduler;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
@@ -21,9 +22,8 @@ import net.minecraftforge.event.world.BlockEvent.NeighborNotifyEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.LogicalSide;
 
-@SuppressWarnings("deprecation")
 public class LeafEvents {
-	private static final Cache<BlockPos, Integer> brokenBlockCache = CacheBuilder.newBuilder().expireAfterWrite(10, TimeUnit.SECONDS).maximumSize(200).build();
+	private static final Cache<BlockPos, Integer> CACHE_BROKEN_BLOCKS = CacheBuilder.newBuilder().expireAfterWrite(10, TimeUnit.SECONDS).maximumSize(256).build();
 	private static final Random R = new Random();
 	
 	@SubscribeEvent
@@ -35,33 +35,66 @@ public class LeafEvents {
 	
 	@SubscribeEvent
 	public void onBreak(BreakEvent e) {
-		if (!(e.getPlayer() instanceof FakePlayer) && !e.getWorld().isClientSide()) {
-			brokenBlockCache.put(e.getPos(), 0);
+		if (!e.getWorld().isClientSide() && !(e.getPlayer() instanceof FakePlayer)) {
+			CACHE_BROKEN_BLOCKS.put(e.getPos(), 0);
 		}
 	}
 	
 	@SubscribeEvent
-	public void onNotifyNeighbors(NeighborNotifyEvent e) { //TODO move this into SBLeaves
-		if (brokenBlockCache.getIfPresent(e.getPos()) != null) {
+	public void onNotifyNeighbors(NeighborNotifyEvent e) {
+		if (CACHE_BROKEN_BLOCKS.getIfPresent(e.getPos()) != null) {
 			ServerWorld world = (ServerWorld) e.getWorld();
-			
 			BlockState notifierState = e.getState();
-			Block b = notifierState.getBlock();
 			
-			if (b.isAir(notifierState, world, e.getPos())) {
-				for (Direction direction : e.getNotifiedSides()) {
-					BlockPos offPos = e.getPos().offset(direction.getNormal());
+			if (notifierState.getBlock().isAir(notifierState, world, e.getPos())) {
+				for (Direction dir : e.getNotifiedSides()) {
+					BlockPos offPos = e.getPos().offset(dir.getNormal());
 					
-					if (world.isLoaded(offPos)) {
-						BlockState state = world.getBlockState(offPos);
-						
-						if (state.getBlock() instanceof SBLeaves) {
-							brokenBlockCache.put(offPos, 0);
-							LeafTickScheduler.schedule(world, offPos, 10 + R.nextInt(5));
-						}
+					if (world.isLoaded(offPos) && world.getBlockState(offPos).getBlock() instanceof SBLeaves) {
+						CACHE_BROKEN_BLOCKS.put(offPos, 0);
+						LeafTickScheduler.PLANNED.add(new ScheduledTick(world, offPos, 10 + R.nextInt(10)));
 					}
 				}
 			}
+		}
+	}
+	
+	private static class LeafTickScheduler {
+		private static final List<ScheduledTick> PLANNED = new ArrayList<ScheduledTick>(), QUEUE = new ArrayList<ScheduledTick>();
+		
+		private static void tick() {
+			if (!PLANNED.isEmpty()) {
+				QUEUE.addAll(PLANNED);
+				PLANNED.clear();
+			}
+			
+			for (Iterator<ScheduledTick> it = QUEUE.iterator(); it.hasNext();) {
+				ScheduledTick st = it.next();
+				
+				if (st.tick-- <= 0) {
+					if (st.world.isLoaded(st.pos)) {
+						BlockState state = st.world.getBlockState(st.pos);
+						if (state.getBlock() instanceof SBLeaves) {
+							state.tick(st.world, st.pos, st.world.getRandom());
+							state.randomTick(st.world, st.pos, st.world.getRandom());
+						}
+					}
+					
+					it.remove();
+				}
+			}
+		}
+	}
+	
+	private static class ScheduledTick {
+		private final ServerWorld world;
+		private final BlockPos pos;
+		private int tick;
+		
+		private ScheduledTick(ServerWorld world, BlockPos pos, int tick) {
+			this.world = world;
+			this.pos = pos;
+			this.tick = tick;
 		}
 	}
 }
